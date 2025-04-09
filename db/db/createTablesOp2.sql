e7ed023fa04670c3a74d46f09dcda6cd347e9605
aozelce <aozelce@madisoncollege.edu>
2025-04-09 15:28:13 -0500

    Team project files

---
+++ db/db/createTablesOp2.sql
@@ -0,0 +1,89 @@
+USE photo_chasers;
+
+#########################################################
+# Remove all tables if they exist, prior to creating them
+#########################################################
+
+DROP TABLE IF EXISTS images;
+DROP TABLE IF EXISTS location_subject;
+DROP TABLE IF EXISTS location;
+DROP TABLE IF EXISTS best_time;
+DROP TABLE IF EXISTS subject;
+
+
+#########################################################
+# Create tables
+#########################################################
+
+CREATE TABLE best_time (
+                         id INTEGER AUTO_INCREMENT PRIMARY KEY,
+                         optimal_time varchar(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE location (
+                      id INTEGER AUTO_INCREMENT PRIMARY KEY,
+                      location_name VARCHAR(255) NOT NULL,
+                      city VARCHAR(100) NOT NULL,
+                      state CHAR(2) NOT NULL,
+                      latitude DECIMAL(9,6) NOT NULL,
+                      longitude DECIMAL(9,6) NOT NULL,
+                      description TEXT NOT NULL,
+                      best_time_id INTEGER NOT NULL,
+                      FOREIGN KEY (best_time_id) REFERENCES best_time(id) ON DELETE RESTRICT,
+                      UNIQUE (location_name, latitude, longitude)
+);
+
+CREATE TABLE subject (
+                         id INTEGER AUTO_INCREMENT PRIMARY KEY,
+                         subject_name varchar(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE location_subject (
+						location_id INT NOT NULL,
+                        subject_id INT NOT NULL,
+                        PRIMARY KEY (location_id, subject_id),
+                        FOREIGN KEY (location_id) REFERENCES location(id) ON DELETE CASCADE,
+                        FOREIGN KEY (subject_id) REFERENCES subject(id) ON DELETE CASCADE
+);
+
+CREATE TABLE images (
+                         id INTEGER AUTO_INCREMENT PRIMARY KEY,
+                         location_id INTEGER NOT NULL,
+                         image_url VARCHAR(500) NOT NULL,
+                         FOREIGN KEY (location_id) REFERENCES location(id) ON DELETE CASCADE,
+                         UNIQUE (location_id, image_url)
+);
+
+###########################
+# Load tables
+###########################
+
+# best_time
+INSERT INTO best_time (optimal_time)
+VALUES ("morning"),
+       ("afternoon"),
+       ("evening"),
+       ("golden hour"),
+       ("night");
+       
+# subjects
+INSERT INTO subject (subject_name)
+VALUES
+    ('Nature'),
+    ('Architecture'),
+    ('Wildlife'),
+    ('Abstract'),
+    ('Cityscape'),
+    ('Historical'),
+    ('Waterfront'),
+    ('Night'),
+    ('Murals'),
+    ('Cultural'),
+    ('Industrial'),
+    ('Parks'),
+    ('Minimalism'),
+    ('Rural'),
+    ('Urban'),
+    ('Seasonal'),
+    ('People');
+    
---
+++ db/src/main/java/com/frameChasers/persistence/GenericDao.java
@@ -0,0 +1,170 @@
+package com.frameChasers.persistence;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import java.util.List;
+
+/**
+ * A generic DAO somewhat inspired by http://rodrigouchoa.wordpress.com
+ */
+
+public class GenericDao<T> {
+    private final Class<T> type;
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+    /**
+     * The Session factory.
+     */
+    SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
+
+    /**
+     * Instantiates a new Generic dao.
+     *
+     * @param type the entity type, for example, User.
+     */
+    public GenericDao(Class<T> type) {
+        this.type = type;
+    }
+
+    /**
+     * Gets all entities
+     *
+     * @return the all entities
+     */
+    public List<T> getAll() {
+        Session session = getSession();
+
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+
+        CriteriaQuery<T> query = builder.createQuery(type);
+        Root<T> root = query.from(type);
+        List<T> list = session.createQuery(query).getResultList();
+        session.close();
+        return list;
+
+    }
+
+    /**
+     * Gets a entity by id
+     * @param id entity id to search by
+     * @return a entity
+     */
+    public <T> T getById(int id) {
+        Session session = getSession();
+        T entity = (T)session.get(type, id);
+        session.close();
+        return entity;
+    }
+
+    /**
+     * Deletes the entity.
+     *
+     * @param entity entity to be deleted
+     */
+    public void delete(T entity) {
+        Session session = getSession();
+        Transaction transaction = session.beginTransaction();
+        session.remove(entity);
+        transaction.commit();
+        session.close();
+    }
+
+    /**
+     * Inserts a new entity into the database.
+     *
+     * @param entity the entity to be inserted
+     * @return the generated ID of the inserted entity
+     */
+    public int insert(T entity) {
+        int id;
+        Session session = getSession();
+        Transaction transaction = session.beginTransaction();
+        session.persist(entity);
+        id = (int) session.getIdentifier(entity);
+        transaction.commit();
+        session.close();
+        return id;
+    }
+
+    /**
+     * Inserts or updates the entity.
+     *
+     * @param entity entity to be inserted/saved
+     */
+    public void update(T entity) {
+        Session session = getSession();
+        Transaction transaction = session.beginTransaction();
+        session.merge(entity);
+        transaction.commit();
+        session.close();
+    }
+
+    /**
+     * Get order by property (exact match)
+     * sample usage: getByPropertyEqual("lastName", "Curry")
+     *
+     * @param propertyName entity property to search by
+     * @param value        value of the property to search for
+     * @return list of orders meeting the criteria search
+     */
+    public List<T> getByPropertyEqual(String propertyName, Object value) {
+        Session session = getSession();
+
+        logger.debug("Searching for entity with " + propertyName + " = " + value);
+
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<T> query = builder.createQuery(type);
+        Root<T> root = query.from(type);
+        query.select(root).where(builder.equal(root.get(propertyName), value));
+        List<T> entities = session.createQuery(query).getResultList();
+
+        session.close();
+        return entities;
+    }
+
+    /**
+     * Get entities by property (partial match)
+     * sample usage: getByPropertyLike("lastName", "Cur")
+     *
+     * @param propertyName entity property to search by
+     * @param value        value of the property to search for (partial match)
+     * @return list of entities meeting the criteria search
+     */
+    public List<T> getByPropertyLike(String propertyName, String value) {
+        Session session = getSession();
+
+        logger.debug("Searching for entity with " + propertyName + " like " + value);
+
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<T> query = builder.createQuery(type);
+        Root<T> root = query.from(type);
+        query.select(root).where(builder.like(root.get(propertyName), "%" + value + "%"));
+        List<T> entities = session.createQuery(query).getResultList();
+
+        session.close();
+        return entities;
+    }
+
+
+
+    /**
+     * Returns an open session from the SessionFactory
+     * @return session
+     */
+    private Session getSession() {
+        return SessionFactoryProvider.getSessionFactory().openSession();
+
+    }
+
+
+
+
+
+}
