4473e0c6e86a9e08d6590ad39d2770813a0622f3
Jamin <jzimmerman15@madisoncollege.edu>
2025-04-10 16:01:19 -0500

    add LocationDaoTest class with basic setup

--- src/test/java/com/frameChasers/persistence/LocationDaoTest.java
+++ src/test/java/com/frameChasers/persistence/LocationDaoTest.java
@@ -1,4 +1,24 @@
 package com.frameChasers.persistence;
 
-public class LocationDaoTest {
+import com.frameChasers.entity.Location;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LocationDaoTest {
+
+    private GenericDao<Location> locationDao;
+
+    @BeforeEach
+    void setUp() {
+        locationDao = new GenericDao<>(Location.class);
+    }
+
+
+
+
+
 }
