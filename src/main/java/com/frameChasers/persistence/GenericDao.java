90f5657dcbc353892bc21f836e3df9aec791b5ba
Jamin <jzimmerman15@madisoncollege.edu>
2025-04-12 00:56:08 -0500

    added javax.persistence.criteria imports to GenericDao for building dynamic queries

--- src/main/java/com/frameChasers/persistence/GenericDao.java
+++ src/main/java/com/frameChasers/persistence/GenericDao.java
@@ -6,9 +6,9 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Root;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 /**
